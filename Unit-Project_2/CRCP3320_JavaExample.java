2854db3106930997d18029bc42a2328fcf165977
tzshepherd <tanner.shepherdsteam@gmail.com>
2024-10-05 11:29:30 -0500

    Add files via upload

---
+++ Unit-Project_2/CRCP3320_JavaExample.java
@@ -0,0 +1,15 @@
+public class CRCP3320_JavaExample {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is some example Java code for a CRCP 3320 assignment in the Fall 2024 Term.");
+        
+        float x = 5.06f;
+        float y = 2.98f;
+        int z = (int)(x * y);
+        System.out.println("z = " + z);
+        
+        if (z <= 15) {
+            System.out.println("math and stuff");
+        }
+    }
+}
