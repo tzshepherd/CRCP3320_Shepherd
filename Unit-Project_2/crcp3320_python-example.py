05591cf1272cd410212299c9d119e063ecc997cf
tzshepherd <tanner.shepherdsteam@gmail.com>
2024-10-05 11:36:58 -0500

    Add files via upload

---
+++ Unit-Project_2/crcp3320_python-example.py
@@ -0,0 +1,9 @@
+print('Hello, World!')
+print('This is some example Python code for a CRCP 3320 assignment in the Fall 2024 Term.')
+
+my_number = 15.8
+their_number = my_number / 4.3
+print('their_number = ', their_number)
+
+if their_number > 5:
+  print('more math')
